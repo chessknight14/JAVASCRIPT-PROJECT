57b0e7ca8b5d7fc9e6cc73155c3399c6722b4a4e
Cireaya <cireayaleffridge@gmail.com>
2025-05-21 12:44:46 -0400

    an error

--- script.js
+++ script.js
@@ -28,4 +28,33 @@ var clock = setInterval(
     }, 1000
 );
 
-//____________________________________
+//____________________________________FLASHLIGHT FUNCTIONS
+
+
+let mouseX = 0;
+let mouseY = 0;
+let flashlight = document.getElementById("flashlight");
+
+const isTouchDevice = () => {
+    //        TO DETECT TOUCHSCREEN DEVICES
+    try {
+        //we try to create touchEvent and it would fail for desktops?
+        document.createEvent("TouchEvent");
+        return true;
+    } catch (e) {
+        return false;
+    }
+};
+
+function getMousePosition(e) {
+    mouseX = !isTouchDevice() ?  e.pageX : e.touches[0].pageX;
+    mouseY = !isTouchDevice() ?  e.pageY : e.touches[0].pageY;
+
+    flashlight.style.setProperty("--Xpos"), mouseX + ("px");
+    flashlight.style.setProperty("--Ypos"), mouseY + ("px");
+}
+
+document.addEventListener("mousemove", getMousePosition);
+document.addEventListener("touchmove", getMousePosition);
+
+
