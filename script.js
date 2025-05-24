802c278cb66e3f87c119b2ee989a64a9bfa54329
Cireaya <cireayaleffridge@gmail.com>
2025-05-24 18:52:57 -0400

    Another project

--- script.js
+++ script.js
@@ -62,8 +62,31 @@ document.addEventListener("touchmove", getMousePosition);
 // window.alert("This is not a drill");
 
 
-//____________________________________OTHER FUNCTIONS____________________________________
+//____________________________________UPLOAD FUNCTIONS____________________________________
 
 
+let fileInput = document.getElementById("file-input");
+let filesList = document.getElementById("files-list");
+let numofFiles = document.getElementById("file-num");
+
+fileInput.addEventListener("change", () => {
+    filesList.innerHTML = "";
+        numofFiles.textContent = `${fileInput.files.length}
+        Files Selected`;
 
 
+    for (i of fileInput.files){
+        let reader = new FileReader();
+        let listItem = document.createElement("li");
+        let fileName = i.name;
+        let fileSize = (i.size / 1024).toFixed(1);
+        listItem.innerHTML = `<p> ${fileName}</p> <p> ${fileSize} KB </p>  `;
+        if (fileSize >= 1024 ){
+            fileSize = (fileSize / 1024 ).toFixed(1);
+            listItem.innerHTML = `<p> ${fileName}</p> <p> ${fileSize} MB </p>  `;
+        }
+
+
+        filesList.appendChild(listItem);
+    }
+});
