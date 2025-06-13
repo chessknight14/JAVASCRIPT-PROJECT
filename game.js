800a6c45850e2ea2deee86036cbe3b04d25b45fc
Cireaya <cireayaleffridge@gmail.com>
2025-06-13 10:09:54 -0400

    Keyboard functions

--- game.js
+++ game.js
@@ -5,6 +5,42 @@ const c = canvas.getContext('2d');
 canvas.width = 600;
 canvas.height = 400;
 
+
+
+let score = 0;
+
+
+
+
+//FUNCTIONS
+
+function bg(){
+	
+	c.fillStyle = "#dac3e2"; // <- ah
+	c.fillRect(0,0, canvas.width, canvas.height);
+
+	//or we could do this in render next time?
+}
+bg();
+
+function drawScore(){
+	c.font = "16px Arial";
+	c.fillStyle = '#fcfcfc';
+	c.fillText(`Score: ${score}`, 5, 20);
+	c.fillText("Hello? hello? hello?", 10, 50);
+}
+
+drawScore();
+
+
+
+
+
+
+
+
+
+
 //        CLASSES
 
 class Player{
@@ -20,22 +56,22 @@ class Player{
 	draw(){ //draw ze person guy
 		c.beginPath();
 		c.arc(this.position.x, this.position.y, 9, 0, 2 * Math.PI);
-		c.fillStyle = '#fcf9fc';
+		c.fillStyle = '#6ecc90';
 		c.fill();
 		
 	}
 	
 	update(){ //update peron's draw and physics
-		const speed = 3;
+		const speed = 5;
 		
 		this.draw();
 		
-		//this.position.x += this.velocity.x;
-		//this.position.y += this.velocity.y;
+		this.position.x += this.velocity.x;
+		this.position.y += this.velocity.y;
 		
 		
 		//player's movement moved?
-		/*if (keys.left.pressed){
+		if (keys.left.pressed){
 			this.velocity.x = -speed;
 		} else if (keys.right.pressed){
 			this.velocity.x = speed;
@@ -49,7 +85,7 @@ class Player{
 		this.velocity.y = speed;
 		} else{
 		this.velocity.y = 0;
-		}*/
+		}
 	
 	
 	} 
@@ -61,28 +97,41 @@ class Player{
 
 
 
-//FUNCTIONS
 
-function bg(){
-	
-	c.fillStyle = "purple"; // <- ah
-	c.fillRect(0,0, canvas.width, canvas.height);
 
-	//or we could do this in render next time?
-}
-bg();
 const guy = new Player();
 
 
+//___________________________KEYS?________________________
+
+
+const keys = {
+	right: {
+	   pressed: false
+	},
+	left: {
+	   pressed: false
+	},
+	up: {
+		pressed: false
+	},
+	down: {
+		pressed: false
+	}
+};
 
 
 
-function render(){
+
+//________________________________________-___GAME LOOP  _________________-__________
+
+function render(){   //ctrl+ ]
 	requestAnimationFrame(render);
 	c.clearRect(0,0, canvas.width, canvas.height); 
 	//draw/update everything \/
 	bg();
     guy.update();
+	drawScore();
 
 
 
@@ -92,36 +141,81 @@ function render(){
 render();
 
 
-/*
-document.addEventListener("keyup", event => {
-    console.log("umm");
-});
+//trying to figure out keyboard functions
 
 
-addEventListener('keydown', event =>  {
-	switch (keyCode){
-		case 38:
-			keys.up.pressed = true;
-			break;
-		case 40:
-			keys.down.pressed = true;
-			break;
-		case 37:
-			keys.left.pressed = true;
-			break;
-		case 39:
-			keys.right.pressed = true;
-			break;
+
+document.addEventListener("keyup", event => {  ///actually works
+	if (event.defaultPrevented){
+		return;
 	}
-	
-})
 
+	switch (event.key) {
+		case "ArrowDown":
+			console.log("ldowneft");
+			break;
+		case "ArrowUp":
+			console.log("up");
+			break;
+		case "ArrowLeft":
+			console.log("left");
+			break;
+		case "ArrowRight":
+			console.log("right");
+			break;
+		default:
+			return;
+
+	}
+
+	event.preventDefault();
+
+	}, 
+	true  //idk
+);
+
+
+document.addEventListener("keydown", event => {  ///actually works
+	if (event.defaultPrevented){
+		return;
+	}
+
+	switch (event.key) {
+		case "ArrowDown":
+			console.log("leftdown");
+			break;
+		case "ArrowUp":
+			console.log("updown");
+			break;
+		case "ArrowLeft":
+			console.log("leftdown");
+			break;
+		case "ArrowRight":
+			console.log("rightdown");
+			break;
+		default:
+			return;
+
+	}
+
+	event.preventDefault();
+
+	}, 
+	true  //idk
+);
+
+
+
+
+/*
 addEventListener('keyup', event =>  {
-	switch (keyCode){
+	switch (event){
 		case 38:
 			
 			keys.up.pressed = false;
+			console.log('key up?');
 			break;
+			
 		case 40:
 			
 			keys.down.pressed = false;
@@ -136,9 +230,5 @@ addEventListener('keyup', event =>  {
 			keys.right.pressed = false;
 			break;
 	}
-	
-
-
 })
-
 */
